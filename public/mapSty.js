574b865109b584060c8b284ecc3710d988c23afd
Jun Ma <junma018@outlook.com>
2022-03-22 20:20:38 +0800

    dev

---
+++ public/mapSty.js
@@ -0,0 +1,52 @@
+var myStyle = {
+    "elements": {
+        "water": { "fillColor": "#a1e0ff" },
+        "waterPoint": { "iconColor": "#a1e0ff" },
+        "transportation": { "strokeColor": "#aa6de0" },
+        "road": { "fillColor": "#b892db" },
+        "railway": { "strokeColor": "#a495b2" },
+        "structure": { "fillColor": "#ffffff" },
+        "runway": { "fillColor": "#ff7fed" },
+        "area": { "fillColor": "#f39ebd" },
+        "political": { "borderStrokeColor": "#fe6850", "borderOutlineColor": "#55ffff" },
+        "point": { "iconColor": "#ffffff", "fillColor": "#FF6FA0", "strokeColor": "#DB4680" },
+        "transit": { "fillColor": "#AA6DE0" }
+    },
+    "version": "1.0" 
+};
+
+var fadedEffect = {
+    "version": "1.0",
+    "settings": {
+        "landColor": "#e7e6e5",
+        "shadedReliefVisible": false
+    },
+    "elements": {
+        "vegetation": {
+            "fillColor": "#c5dea2"
+        },
+        "naturalPoint": {
+            "visible": false,
+            "labelVisible": false
+        },
+        "transportation": {
+            "labelOutlineColor": "#ffffff",
+            "fillColor": "#ffffff",
+            "strokeColor": "#d7d6d5"
+        },
+        "water": {
+            "fillColor": "#b1bdd6",
+            "labelColor": "#ffffff",
+            "labelOutlineColor": "#9aa9ca"
+        },
+        "structure": {
+            "fillColor": "#d7d6d5"
+        },
+        "indigenousPeoplesReserve": {
+            "visible": false
+        },
+        "military": {
+            "visible": false
+        }
+    }
+};
