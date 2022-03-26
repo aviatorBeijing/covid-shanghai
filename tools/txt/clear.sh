1b84120d2fa3d40554c5e415e03080e4ccd33b91
Jun Ma <junma018@outlook.com>
2022-03-26 11:32:26 +0800

    dev: update date

--- tools/txt/clear.sh
+++ tools/txt/clear.sh
@@ -3,10 +3,10 @@
 INPUTF=$1/cases-nosyndrome.txt
 OUTPUTF=$1/cases-nosyndrome-clear.txt 
 
-sed 's/无症状感染者[0-9]*/|/g' $INPUTF | tr '|' '\n' | sed '/间新冠病毒核/d' | sed 's/　//g' | sed 's/，/,/g' | sed 's/^,//g' | sed 's/,$//g' | sed 's/。/./g' | sed 's/居住于//g' | sed '/^$/d' | sed '/^$/d' | awk -F"," '{print $1,$2,$3}' > $OUTPUTF
+sed 's/无症状感染者[0-9]*/|/g' $INPUTF | tr '|' '\n' | sed '/间新冠病毒核/d' | sed 's/　//g' | sed 's/，/,/g' | sed 's/^,//g' | sed 's/,$//g' | sed 's/。/./g' | sed 's/居住于//g' | sed '/^$/d' | sed '/^$/d' | awk -F"," '{print $1,$2,$3}' | sort > $OUTPUTF
 
 INPUTF=$1/cases-confirmed.txt
 OUTPUTF=$1/cases-confirmed-clear.txt 
 
-sed 's/病例[0-9]*/|/g' $INPUTF | tr '|' '\n' | sed '/间新冠病毒核/d' | sed 's/　//g' | sed 's/，/,/g' | sed 's/^,//g' | sed 's/,$//g' | sed 's/。/./g' | sed 's/居住于//g' | sed '/^$/d' | sed '/^$/d' | awk -F"," '{print $1,$2,$3}' > $OUTPUTF
+sed 's/病例[0-9]*/|/g' $INPUTF | tr '|' '\n' | sed '/间新冠病毒核/d' | sed 's/　//g' | sed 's/，/,/g' | sed 's/^,//g' | sed 's/,$//g' | sed 's/。/./g' | sed 's/居住于//g' | sed '/^$/d' | sed '/^$/d' | awk -F"," '{print $1,$2,$3}' | sort > $OUTPUTF
 
