d89d30d004e1639ad23cbeaa73712102b29874e5
Jun Ma <junma018@outlook.com>
2022-04-13 10:26:07 +0800

    dev:data

--- tools/txt/loction_only.sh
+++ tools/txt/loction_only.sh
@@ -5,13 +5,16 @@ sed 's/。/./g' $1/$RAW_FILE | tr ',' '\n' | sed 's/、$//g' | sed 's/，$//g' |
 python merg.py x | sort | uniq > $1/$OUTPUT_FILE
 rm x
 
+cp $1/$OUTPUT_FILE $1/$OUTPUT_FILE.v0
+
 cat \
-	./20220407/cases-location-clear.txt \
-	./20220408/cases-location-clear.txt \
-	./20220409/cases-location-clear.txt \
-	./20220410/cases-location-clear.txt \
-	./20220411/cases-location-clear.txt \
-	./20220412/cases-location-clear.txt \
-	./20220413/cases-location-clear.txt | \
+	./20220407/cases-location-clear.txt.v0 \
+	./20220408/cases-location-clear.txt.v0 \
+	./20220409/cases-location-clear.txt.v0 \
+	./20220410/cases-location-clear.txt.v0 \
+	./20220411/cases-location-clear.txt.v0 \
+	./20220412/cases-location-clear.txt.v0 \
+	./20220413/cases-location-clear.txt.v0 | \
 	sort | uniq > $1/$OUTPUT_FILE.v1
 
+cp $1/$OUTPUT_FILE.v1 $1/$OUTPUT_FILE
